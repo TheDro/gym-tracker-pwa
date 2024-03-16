73a63c0f55e2d891b01f5aa4dc9a860fd664d203
TheDro <andrewsc32@protonmail.com>
2024-03-16 08:36:54 -0400

    Use alternative file import API

--- src/helpers/file_helper.js
+++ src/helpers/file_helper.js
@@ -21,13 +21,38 @@ function saveAsFile(data, filename = 'data.json') {
 
 function loadJsonFile() {
     // Note: `showOpenilePicker` is not supported in Brave
-    return window.showOpenFilePicker().then(fileHandle => {
-        return fileHandle[0].getFile()
-    }).then(file => {
+    return _showOpenFilePicker().then(file => {
         return file.text()
     }).then(text => {
         return JSON.parse(text)
     })
 }
+
+
+function _showOpenFilePicker() {
+    let input = document.createElement('input')
+    input.type = 'file'
+    input.multiple = false
+    input.accept = '.json'
+
+    input.style.position = 'fixed'
+    input.style.top = '-10000px'
+    input.style.left = '-10000px'
+    document.body.appendChild(input)
+
+    return new Promise((resolve, reject) => {
+        input.addEventListener('change', () => {
+            let file = input.files[0]
+            if (!file) {
+                reject()
+            } else {
+                resolve(file)
+            }
+            document.body.removeChild(input)
+        })
+        input.click()
+    })
+}
+
 
 export {saveAsFile, loadJsonFile}
